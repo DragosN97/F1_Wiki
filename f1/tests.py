3975364662c172748313cf88f22af9462e351ede
DragosN97 <dragos.orasanu9@gmail.com>
2025-09-11 15:02:19 +0300

    Tests added and register page photo changed

--- f1/tests.py
+++ f1/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+import pytest
+from f1.models import Driver
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+@pytest.fixture
+def user(db):
+    return get_user_model().objects.create_user(username='Dragos1', password='Tomato123!@#')
+
+@pytest.fixture
+def dogged_in_client(client, user):
+    client.login(username='Dragos1', password='Tomato123!@#')
+    return client
+
+def test_driver_creation(db, user):
+    saved_driver  = Driver.objects.create(name='Valteri Botas', team='Mercedes', age=33, created_by=user )
+
+    db_driver = Driver.objects.first()
+    assert saved_driver.title = db_driver.title
+
+
